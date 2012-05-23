16b89105f0b21f15982e25f91a2a823b9c82b71f
Eugenio Culurciello <euge@purdue.edu>
2012-05-23 11:42:15 -0400

    updated and fixed a few bugs in SAD algo test

--- SADtest_v2.lua
+++ SADtest_v2.lua
@@ -1,6 +1,6 @@
 -- May 10th 2012
 -- EC test on sum of absolute differences (SAD) algorithm
--- test on TLD video dataset: full frame processing
+-- test on TLD video dataset: search-region bounded algorithm (more efficient)
 
 -- motocross lost on frame 27 with a 31x31 kernel at y_init,x_init=40,300
 
@@ -70,61 +70,6 @@ SADbw = inline.load [[
       return 0;
 ]]
 
-SADcolor = inline.load [[
-      // get args
-      const void* id = luaT_checktypename2id(L, "torch.FloatTensor");
-      THFloatTensor *input = luaT_checkudata(L, 1, id);
-      THFloatTensor *kernel = luaT_checkudata(L, 2, id);
-      THFloatTensor *output = luaT_checkudata(L, 3, id);
-      
-      // get raw pointers
-      float *input_data = THFloatTensor_data(input);
-      float *kernel_data = THFloatTensor_data(kernel);
-      float *output_data = THFloatTensor_data(output);
-      
-	  //get dims:
-	  int it = input->size[0];
-	  int ir = input->size[1];
-      int ic = input->size[2];
-      int kt = kernel->size[0];
-      int kr = kernel->size[1];
-      int kc = kernel->size[2];
-	  int ot = output->size[0]-1;
-	  //printf("o=%g ", ot);
-	  int or = output->size[1];//(ir - kr) + 1;
-	  int oc = output->size[2];//(ic - kc) + 1;
-	
-	  int zz, xx, yy;
-	  
-	  for (zz = 0; zz < ot; zz++)
-	  {
-	    for(yy = 0; yy < or; yy++)
-	    {
-	      for(xx = 0; xx < oc; xx++)
-	      {
-	        /* Dot product in two dimensions... (between input image and the mask) */
-	        float *pi_ = input_data + zz*ir*ic + yy*ic + xx;
-	        float *pw_ = kernel_data;
-	        float sum = 0;
-	        int kz, kx, ky;
-	        for(kz = 0; kz < kt; kz++)
-	        {
-	          for(ky = 0; ky < kr; ky++)
-	          {
-	            for(kx = 0; kx < kc; kx++) {
-	              sum += abs(pi_[kx]-pw_[kx]);
-	            }
-	            pi_ += ic; /* next input line */
-	            pw_ += kc; /* next mask line */
-	          }
-	        }
-	        /* Update output */
-	        *output_data++ += sum;
-	      }
-	    }
-	  }
-      return 0;
-]]
 
 -- function max
 function GetMax(a)
@@ -148,8 +93,8 @@ p = xlua.Profiler()
 -- input image/test:
 inputim=image.loadPNG('/Users/eugenioculurciello/AdvancedResearch/SyntheticVision/datasets/TLD/07_motocross/00001.png',3)
 -- initial object/patch location and video frame
-px=300
-py=40
+px=285
+py=25
 i=1
 
 -- global linear normalization of input frame
@@ -171,62 +116,91 @@ kc = fil_c
 our = (ir - kr) + 1
 ouc = (ic - kc) + 1
 
+-- search range for SAD maxima/min
+src_rng=100
+if src_rng < fil2r then print("Error: search range src_rng < patch size/2") end
+
 -- inits:
 processed=torch.zeros(ir,ic)
-total=torch.zeros(our, ouc) -- outputs
-temp1=torch.zeros(our, ouc)
-temp2=torch.zeros(our, ouc)
-temp3=torch.zeros(our, ouc)
 outim=torch.zeros(inputim:size())
 ker=torch.zeros(inputim:size(1), fil_r, fil_c)
 ker=image.crop(Ninputim, px-fil2c, py-fil2r, px+fil2c+1, py+fil2r+1)
-
 
 -- loop on video frames:
 function process()
 	inputim=image.loadPNG(string.format(
 		"/Users/eugenioculurciello/AdvancedResearch/SyntheticVision/datasets/TLD/07_motocross/%0005d", i) .. '.png',3)
+	print('Frame count:'..i)
 
 	-- global linear normalization of input frame
 	kNorm = torch.ones(5) --gaussian = image.gaussian(3,0.15)
 	m=nn.SpatialSubtractiveNormalization(3,kNorm)
 	Ninputim = m:forward(inputim)
 	
-	ym = fil2r -- outputs box (shrinked from input box)
-	yM = ir-fil2r-1
-	xm = fil2c
-	xM = ic-fil2c-1
-	--print(ym,yM,xm,xM)
-
+	--ker=image.crop(Ninputim, px, py, px+fil_s, py+fil_s) -- test ONLY!!!!
 	-- SAD: REMEMBER TO INIT OUTput to 0!!!
-	temp1:mul(0)
-	temp2:mul(0)
-	temp3:mul(0)	
-	SADbw(Ninputim[1],ker[1],temp1)
-	SADbw(Ninputim[2],ker[2],temp2)
-	SADbw(Ninputim[3],ker[3],temp3)
+	processed=processed:mul(0)
+	
+	y1=math.max(py-src_rng,1) -- inputs box: bounded (src_rng) around (px,py) 
+	y2=math.min(py+src_rng,ir)
+	x1=math.max(px-src_rng,1)
+	x2=math.min(px+src_rng,ic)
+	
+	tim1=Ninputim[1]:sub(y1,y2,x1,x2) -- inputs
+	tim2=Ninputim[2]:sub(y1,y2,x1,x2)
+	tim3=Ninputim[3]:sub(y1,y2,x1,x2)
+	
+	nir = tim1:size(1)
+	nic = tim1:size(2)
+	nour = (nir - kr) + 1
+	nouc = (nic - kc) + 1
+	
+	ym = y1+fil2r -- outputs box (shrinked from input box)
+	yM = y2-fil2r
+	xm = x1+fil2c
+	xM = x2-fil2c
+	
+	total=torch.zeros(nour, nouc) -- outputs
+	temp1=torch.zeros(nour, nouc)
+	temp2=torch.zeros(nour, nouc)
+	temp3=torch.zeros(nour, nouc)
+	
+	--print(y1,y2,x1,x2)
+	--print(ym,yM,xm,xM)
+	--print(tim1:size(),temp1:size()) 
 
+	SADbw(tim1,ker[1],temp1)
+	SADbw(tim2,ker[2],temp2)
+	SADbw(tim3,ker[3],temp3)
+	
 	total=temp1+temp2+temp3
 	total=total/torch.max(total)
 	total = total:mul(-1):add(1)
 	--print(total:size())
+	--print(processed:size())
 	processed[{{ym,yM},{xm,xM}}] = total
+	--processed=total
 	outim[1]:copy(processed)
 	outim[2]:copy(processed)
 	outim[3]:copy(processed)
-
+	
 	-- update object/patch location
-	value, px_nxt, py_nxt = GetMax(processed)
+	value, px_nxt, py_nxt = GetMax(total)
 	print('SAD output:', value)
-    py=py_nxt
-    px=px_nxt
-
+	print(px_nxt,py_nxt, x1,y1)
+	
+	px = px_nxt-1 + x1-1 + fil2c
+    py = py_nxt-1 + y1-1 + fil2r
+    print(px,py)
+   
     -- next patch extraction
 	ker=image.crop(Ninputim, px-fil2c, py-fil2r, px+fil2c+1, py+fil2r+1)
-	
+    
+    --if i==1 then image.display(total) end
     -- continue loop, chose endframe
-    if i==100 then i=1 else i=i+1 end
-    print('Frame count:'..i)
+    if i==1 then i=1 else i=i+1 end
+    -- close program window at a certain frame if needed:
+    --if i==10 then exit() end
 end	
 
 -- display function
@@ -241,9 +215,9 @@ function display()
 
    -- (2) overlay bounding boxes for each detection
    win:setcolor(1,0,0)
-   win:rectangle(px-fil2c, py-fil2r, fil_c, fil_r)
-   win:rectangle(ic+px-fil2c, py-fil2r, fil_c, fil_r)
-   win:rectangle(2*ic+px-fil2c, py-fil2r, fil_c, fil_r)
+   win:rectangle(px, py, fil_c, fil_r)
+   win:rectangle(ic+px, py, fil_c, fil_r)
+   --win:rectangle(2*ic+px, py, fil_c, fil_r)
    win:stroke()
    --win:setfont(qt.QFont{serif=false,italic=false,size=16})
    --win:moveto(detect.x, detect.y-1)
@@ -277,3 +251,7 @@ qt.connect(timer,
 widget.windowTitle = 'SAD tracker'
 widget:show()
 timer:start()
+
+	
+
+
