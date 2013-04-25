627b3b1d9866ed3506ca8b7fe23074045dc49114
Eugenio Culurciello <euge@purdue.edu>
2013-04-25 10:54:34 -0400

    update

--- multinet/data_person.lua
+++ multinet/data_person.lua
@@ -36,19 +36,24 @@ function ls(path) return sys.split(sys.ls(path),'\n') end -- alf ls() nice funct
 ----------------------------------------------------------------------
 -- load or generate new dataset:
 
-if paths.filep('train.t7') and paths.filep('test.t7') then
+if paths.filep('../../datasets/INRIAPerson/train.t7') 
+		and paths.filep('../../datasets/INRIAPerson/test.t7') then
 
 	print '==> loading previously generated dataset:'
-	trainData = torch.load('train.t7')
-	testData = torch.load('test.t7')
+	trainData = torch.load('../../datasets/INRIAPerson/train.t7')
+	testData = torch.load('../../datasets/INRIAPerson/test.t7')
+	
+	trsize = trainData.data:size(1)
+	tesize = testData.data:size(1)
 
 else
 
-	print '==> creating a new dataset from rawa images:'
+	print '==> creating a new dataset from raw files:'
 
 	-- video dataset to get background from:
 	dspath = '../../datasets/driving1.mov'
-	source = ffmpeg.Video{path=dspath, width = 160, height = 120, encoding='jpg', fps=24, loaddump=false, load=false}
+	source = ffmpeg.Video{path=dspath, width = 160, height = 120, encoding='jpg', 
+			fps=24, loaddump=false, load=false}
 
 	rawFrame = source:forward()
 	-- input video params:
@@ -58,45 +63,45 @@ else
 	source.current = 1 -- rewind video frames
 
 	ivch = 3 -- color channels in images
-	des_ima_w = 32 -- desired cropped dataset image size
-	des_ima_h = 32
+	desImaX = 32 -- desired cropped dataset image size
+	desImaY = 32
 
-	crop_tr_x = 50 -- desired offset to crop images from train set
-	crop_tr_y = 45
-	crop_te_x = 33 -- desired offset to crop images from test set
-	crop_te_y = 30
+	cropTrX = 50 -- desired offset to crop images from train set
+	cropTrY = 45
+	cropTeX = 33 -- desired offset to crop images from test set
+	cropTeY = 30
 
-	label_person = 1 -- label for person and background:
-	label_bg = 0
+	labelPerson = 1 -- label for person and background:
+	labelBg = 0
 
-	traindir='../../datasets/INRIAPerson/96X160H96/Train/pos/'
-	train_ima_number = #ls(traindir)
-	testdir='../../datasets/INRIAPerson/70X134H96/Test/pos/'
-	test_ima_number = #ls(testdir)
+	trainDir='../../datasets/INRIAPerson/96X160H96/Train/pos/'
+	trainImaNumber = #ls(traindir)
+	testDir='../../datasets/INRIAPerson/70X134H96/Test/pos/'
+	testImaNumber = #ls(testdir)
 
 	-- dataset size:
-	data_multiplier = 1 -- optional: take multiple samples per image: +/- 2 pix H, V = 4 total
-	trsize = data_multiplier * train_ima_number
-	tesize = data_multiplier * test_ima_number
+	dataMultiplier = 1 -- optional: take multiple samples per image: +/- 2 pix H, V = 4 total
+	trsize = dataMultiplier * trainImaNumber
+	tesize = dataMultiplier * testImaNumber
 
 	trainData = {
-		data = torch.Tensor(trsize, ivch,des_ima_w,des_ima_h),
+		data = torch.Tensor(trsize, ivch,desImaX,desImaY),
 		labels = torch.Tensor(trsize),
 		size = function() return trsize end
 	}
 
 	-- load person data:
 	for i = 1, train_ima_number, 2 do
-		imatoload = image.loadPNG(traindir..ls(traindir)[i],ivch)
-		trainData.data[i] = image.crop(imatoload, crop_tr_x-des_ima_w/2, crop_tr_y-des_ima_h/2, 
-																crop_tr_x+des_ima_w/2, crop_tr_y+des_ima_h/2):clone()
+		imatoload = image.loadPNG(traindir..ls(trainDir)[i],ivch)
+		trainData.data[i] = image.crop(imatoload, cropTrX-desImaX/2, cropTrY-desImaY/2, 
+																cropTrX+desImaX/2, cropTrY+desImaY/2):clone()
 		trainData.labels[i] = label_person
 	
 		-- load background data:
 		imatoload = source:forward()
-		local x = math.random(1,ivwi-des_ima_w+1)
-		local y = math.random(20,ivhe-des_ima_h+1-40) -- added # to get samples more or less from horizon
-		trainData.data[i+1] = imatoload[{ {},{y,y+des_ima_h-1},{x,x+des_ima_w-1} }]:clone()
+		local x = math.random(1,ivwi-desImaX+1)
+		local y = math.random(20,ivhe-desImaY+1-40) -- added # to get samples more or less from horizon
+		trainData.data[i+1] = imatoload[{ {},{y,y+desImaY-1},{x,x+desImaX-1} }]:clone()
 		trainData.labels[i+1] = label_bg
 	end
 
@@ -104,23 +109,23 @@ else
 
 
 	testData = {
-		data = torch.Tensor(tesize, ivch,des_ima_w,des_ima_h),
+		data = torch.Tensor(tesize, ivch,desImaX,desImaY),
 		labels = torch.Tensor(tesize),
 		size = function() return tesize end
 	}
 
 	-- load person data:
 	for i = 1, test_ima_number, 2 do
-		imatoload = image.loadPNG(testdir..ls(testdir)[i],ivch)
-		testData.data[i] = image.crop(imatoload, crop_te_x-des_ima_w/2, crop_te_y-des_ima_h/2, 
-																crop_te_x+des_ima_w/2, crop_te_y+des_ima_h/2):clone()
+		imatoload = image.loadPNG(testdir..ls(testDir)[i],ivch)
+		testData.data[i] = image.crop(imatoload, cropTeX-desImaX/2, cropTeY-desImaY/2, 
+																cropTeX+desImaX/2, cropTeY+desImaY/2):clone()
 		testData.labels[i] = label_person
 	
 		-- load background data:
 		imatoload = source:forward()
-		local x = math.random(1,ivwi-des_ima_w+1)
-		local y = math.random(20,ivhe-des_ima_h+1-40) -- added # to get samples more or less from horizon
-		testData.data[i+1] = imatoload[{ {},{y,y+des_ima_h-1},{x,x+des_ima_w-1} }]:clone()
+		local x = math.random(1,ivwi-desImaX+1)
+		local y = math.random(20,ivhe-desImaY+1-40) -- added # to get samples more or less from horizon
+		testData.data[i+1] = imatoload[{ {},{y,y+desImaY-1},{x,x+desImaX-1} }]:clone()
 		testData.labels[i+1] = label_bg
 	end
 
@@ -166,13 +171,13 @@ testData.data = testData.data:float()
 --     as a result, each color component has 0-mean and 1-norm across the dataset.
 
 -- Convert all images to YUV
---print '==> preprocessing data: colorspace RGB -> YUV'
---for i = 1,trsize do
---   trainData.data[i] = image.rgb2yuv(trainData.data[i])
---end
---for i = 1,tesize do
---   testData.data[i] = image.rgb2yuv(testData.data[i])
---end
+print '==> preprocessing data: colorspace RGB -> YUV'
+for i = 1,trsize do
+   trainData.data[i] = image.rgb2yuv(trainData.data[i])
+end
+for i = 1,tesize do
+   testData.data[i] = image.rgb2yuv(testData.data[i])
+end
 
 -- Name channels for convenience
 channels = {'y','u','v'}
@@ -215,13 +220,13 @@ normalization = nn.SpatialContrastiveNormalization(1, neighborhood, 1e-3):float(
 
 -- Normalize all channels locally:
 --for c in ipairs(channels) do
---c = 1
---   for i = 1,trsize do
---      trainData.data[{ i,{c},{},{} }] = normalization:forward(trainData.data[{ i,{c},{},{} }])
---   end
---   for i = 1,tesize do
---      testData.data[{ i,{c},{},{} }] = normalization:forward(testData.data[{ i,{c},{},{} }])
---   end
+c = 1
+   for i = 1,trsize do
+      trainData.data[{ i,{c},{},{} }] = normalization:forward(trainData.data[{ i,{c},{},{} }])
+   end
+   for i = 1,tesize do
+      testData.data[{ i,{c},{},{} }] = normalization:forward(testData.data[{ i,{c},{},{} }])
+   end
 --end
 
 ----------------------------------------------------------------------
