b9f855b0fe27290cb5e42f0fef72be86dc869ce8
Alfredo Canziani <alfredo.canziani@gmail.com>
2013-05-07 21:00:41 -0400

    Created data-signal.lua
       At the moment it just show the signal dataset in order to find the better
       way to handle it.

---
+++ multinet/data-signal.lua
@@ -0,0 +1,26 @@
+------------------------------------------------------------------------------
+-- Loading street signals
+------------------------------------------------------------------------------
+-- Alfredo Canziani May 2013
+------------------------------------------------------------------------------
+
+-- Requires ------------------------------------------------------------------
+require 'image'
+require 'sys'
+
+-- Useful functions ----------------------------------------------------------
+function ls(path) return sys.split(sys.ls(path),'\n') end -- alf ls() nice function!
+
+-- Parameters ----------------------------------------------------------------
+path = '../../datasets/GTSRB/Final_Training/Images/'
+--sign = sign or 2
+for sign = 1, #ls(path) do
+
+   -- Showing a couple of images
+   for i = 1,#ls(path .. ls(path)[sign] .. '/*.png'),5 do
+      img = image.load(ls(path .. ls(path)[sign] .. '/*.png')[i])
+      win = image.display{image=img,zoom=10,win=win}
+      --io.read()
+   end
+
+end
