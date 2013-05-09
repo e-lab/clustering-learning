e2c6f42f174c3554f16dfae061206b65510b85b8
Alfredo Canziani <alfredo.canziani@gmail.com>
2013-05-08 23:43:52 -0400

    Calculated trSize (first- and last-frame command-line parameters)

--- multinet/data-sign.lua
+++ multinet/data-sign.lua
@@ -18,15 +18,27 @@ if not opt then
    print '==> Processing options'
    opt = lapp [[
    --playDataset visualise the whole signs dataset
+   --firstFrame (default 21)
+   --lastFrame  (default 30)
 ]]
 end
 
 -- Parameters ----------------------------------------------------------------
 ds = eex.datasetsPath()
 path = ds .. 'GTSRB/Final_Training/Images/'
-print(path)
 
 -- Main program -------------------------------------------------------------
+print '==> creating a new dataset from raw files:'
+totNbSign = 0
+for i = 1, #ls(path) do
+   nbSign = #ls(path..ls(path)[i]..'/*.png')/30
+   totNbSign = totNbSign + nbSign
+end
+trSize = totNbSign * (opt.lastFrame - opt.firstFrame + 1)
+print(trSize)
+
+
+
 -- Play the dataset, if requested
 if opt.playDataset then
    print 'Visualising the dataset'
