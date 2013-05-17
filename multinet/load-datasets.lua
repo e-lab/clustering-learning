c7ba6ed7a92da6c33d03cb3d7295e4779a7d42c1
Alfredo Canziani <alfredo.canziani@gmail.com>
2013-05-17 17:37:30 -0400

    Introduced <Main IF> for loading (instead of creating) the dataset
    If <{test,train}.t7> exit in the <.> directory => they are loaded.
    If there is the need of recraft the dataset (with different parameter)
    then the <t7> files have to be deleted

--- multinet/load-datasets.lua
+++ multinet/load-datasets.lua
@@ -7,61 +7,86 @@
 -- Parameters ----------------------------------------------------------------
 torch.setnumthreads(3)
 
--- Global functions ----------------------------------------------------------
-function cat(a,b)
-   if a and b then
-      return torch.cat(a,b,1)
-   else
-      return a or b
+-- Main IF -------------------------------------------------------------------
+if paths.filep('train.t7') and paths.filep('test.t7') then
+
+   print '==> loading previously generated dataset:'
+   trainData = torch.load('train.t7')
+   testData = torch.load('test.t7')
+
+else
+
+   -- Global functions ----------------------------------------------------------
+   function cat(a,b)
+      if a and b then
+         return torch.cat(a,b,1)
+      else
+         return a or b
+      end
    end
+
+   -- Main program -------------------------------------------------------------
+   -- Temporary dataset container
+   local totalTrainData = {}
+   local totalTestData = {}
+
+   -- Datasets' name
+   local datasets = {
+      'data-person',
+      'data-sign',
+      'data-kitti'
+   }
+
+   -- Number of classes per dataset
+   local classes = {
+      1,
+      43,
+      1
+   }
+
+   -- Computing cumulative number of classes
+   local totNbClasses = {0}
+   for i,c in ipairs(classes) do
+      totNbClasses[i+1] = totNbClasses[i] + c
+   end
+
+   -- Cleaning the screen before executing (a lot of text out will follow)
+   os.execute('clear')
+
+   -- Concatenating all datasets contained in <dataset> table
+   for i,d in ipairs(datasets) do
+
+      local data = require(d)
+
+      totalTrainData.data = cat(totalTrainData.data,data.trainData.data)
+      totalTrainData.labels = cat(totalTrainData.labels,data.trainData.labels + 1 + totNbClasses[i])
+      totalTrainData.size = function() return (#totalTrainData.data)[1] end
+
+      totalTestData.data = cat(totalTestData.data,data.testData.data)
+      totalTestData.labels = cat(totalTestData.labels,data.testData.labels + 1 + totNbClasses[i])
+      totalTestData.size = function() return (#totalTestData.data)[1] end
+
+   end
+
+   -- Exporting main dataset to the global namespace
+   trainData = totalTrainData
+   testData  = totalTestData
+
+   -- Printing the category statistics ------------------------------------------
+   print '==> showing class statistics'
+   gnuplot.figure(1)
+   gnuplot.hist(trainData.labels,totNbClasses[#totNbClasses])
+   gnuplot.title('Training dataset category statistics')
+   gnuplot.figure(2)
+   gnuplot.hist(testData.labels,totNbClasses[#totNbClasses])
+   gnuplot.title('Testing dataset category statistics')
+
+   -- Save created dataset ------------------------------------------------------
+   print '==> saving dataset on disk'
+   torch.save('train.t7',trainData)
+   torch.save('test.t7',testData)
+
 end
 
--- Main program -------------------------------------------------------------
--- Temporary dataset container
-local totalTrainData = {}
-local totalTestData = {}
-
--- Datasets' name
-local datasets = {
-   'data-person',
-   'data-sign',
-   'data-kitti'
-}
-
--- Number of classes per dataset
-local classes = {
-   1,
-   43,
-   1
-}
-
--- Computing cumulative number of classes
-local totNbClasses = {0}
-for i,c in ipairs(classes) do
-   totNbClasses[i+1] = totNbClasses[i] + c
-end
-
--- Cleaning the screen before executing (a lot of text out will follow)
-os.execute('clear')
-
--- Concatenating all datasets contained in <dataset> table
-for i,d in ipairs(datasets) do
-
-   local data = require(d)
-
-   totalTrainData.data = cat(totalTrainData.data,data.trainData.data)
-   totalTrainData.labels = cat(totalTrainData.labels,data.trainData.labels + 1 + totNbClasses[i])
-   totalTrainData.size = function() return (#totalTrainData.data)[1] end
-
-   totalTestData.data = cat(totalTestData.data,data.testData.data)
-   totalTestData.labels = cat(totalTestData.labels,data.testData.labels + 1 + totNbClasses[i])
-   totalTestData.size = function() return (#totalTestData.data)[1] end
-
-end
-
--- Exporting main dataset to the global namespace
-trainData = totalTrainData
-testData  = totalTestData
-
--- Done :)
+-- Done :) -------------------------------------------------------------------
 print '==> Datasets loaded successfully! :)'
