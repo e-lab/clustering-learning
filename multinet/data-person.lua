06e1cae78d02f837a4ca2785d43d9f53e04bc16c
Alfredo Canziani <alfredo.canziani@gmail.com>
2013-07-10 13:08:44 -0400

    Generate 12x images for <data-person>

--- multinet/data-person.lua
+++ multinet/data-person.lua
@@ -56,9 +56,9 @@ local testDir = path .. '70X134H96/Test/pos/'
 local testImaNumber = #ls(testDir)
 
 -- dataset size:
-local dataMultiplier = 1 -- optional: take multiple samples per image: +/- 2 pix H, V = 4 total
-trSize = dataMultiplier * trainImaNumber / 2
-teSize = dataMultiplier * testImaNumber / 2
+local dataMultiplier = 6 -- optional: take multiple samples per image: +/|/- 5px H, |/+ 5px V, mirror = 3x2x2 = 12 total
+trSize = dataMultiplier * trainImaNumber / 1 -- 1 == mirror, 2 == no mirror
+teSize = dataMultiplier * testImaNumber / 1
 
 -- Main program -------------------------------------------------------------
 print '==> creating a new training dataset from raw files:'
@@ -71,12 +71,17 @@ trainData = {
 
 -- load person data:
 local idx = 0
-for i = 1, trainImaNumber, 2 do
-   idx = idx + 1
-   img = image.loadPNG(trainDir..ls(trainDir)[i],ivch)
-   trainData.data[idx] = image.scale(image.crop(img, cropTrX-(desImaX-10)/2, cropTrY-(desImaY-10)/2, cropTrX+(desImaX-10)/2, cropTrY+(desImaY-10)/2),desImaX,desImaY)
-   trainData.labels[idx] = labelPerson
-   xlua.progress(idx,trSize)
+for i = 1, trainImaNumber, 1 do
+   local img = image.loadPNG(trainDir..ls(trainDir)[i],ivch)
+   for cropTrX = cropTrX-5, cropTrX+5, 5 do
+      for cropTrY = cropTrY, cropTrY+5, 5 do
+         idx = idx + 1
+         -- trainData.data[idx] = image.scale(image.crop(img, cropTrX-(desImaX-10)/2, cropTrY-(desImaY-10)/2, cropTrX+(desImaX-10)/2, cropTrY+(desImaY-10)/2),desImaX,desImaY)
+         trainData.data[idx] = image.crop(img, cropTrX-desImaX/2, cropTrY-desImaY/2, cropTrX+desImaX/2, cropTrY+desImaY/2)
+         trainData.labels[idx] = labelPerson
+         xlua.progress(idx,trSize)
+      end
+   end
 end
 -- display some examples:
 image.display{image=trainData.data[{{1,128}}], nrow=16, zoom=2, legend = 'Train Data'}
@@ -91,15 +96,50 @@ testData = {
 
 -- load person data:
 idx = 0
-for i = 1, testImaNumber, 2 do
-   idx = idx + 1
+for i = 1, testImaNumber, 1 do
    local img = image.loadPNG(testDir..ls(testDir)[i],ivch)
-   testData.data[idx] = image.scale(image.crop(img, cropTeX-(desImaX-10)/2, cropTeY-(desImaY-10)/2, cropTeX+(desImaX-10)/2, cropTeY+(desImaY-10)/2),desImaX,desImaY)
-   testData.labels[idx] = labelPerson
-   xlua.progress(idx,teSize)
+   for cropTeX = cropTeX-5, cropTeX+5, 5 do
+      for cropTeY = cropTeY, cropTeY+5, 5 do
+         idx = idx + 1
+         -- testData.data[idx] = image.scale(image.crop(img, cropTeX-(desImaX-10)/2, cropTeY-(desImaY-10)/2, cropTeX+(desImaX-10)/2, cropTeY+(desImaY-10)/2),desImaX,desImaY)
+         testData.data[idx] = image.crop(img, cropTeX-desImaX/2, cropTeY-desImaY/2, cropTeX+desImaX/2, cropTeY+desImaY/2)
+         testData.labels[idx] = labelPerson
+         xlua.progress(idx,teSize)
+      end
+   end
 end
 -- display some examples:
 image.display{image=testData.data[{{1,128}}], nrow=16, zoom=2, legend = 'Test Data'}
+
+-- Shuffling the datasets and uniforming the statistics ---------------------
+print '==> Uniforming statistics: #training = 14040, #testing = 4860 (such as for <stop>)'
+local shuffle = torch.randperm(trainData:size())
+oldTrainData = trainData
+trSize = 14040
+trainData = {
+   data = torch.Tensor(trSize, ivch,desImaX,desImaY),
+   labels = torch.Tensor(trSize),
+   size = function() return trSize end
+}
+for i = 1,trSize do
+   trainData.data[i] = oldTrainData.data[shuffle[i]]
+   trainData.labels[i] = oldTrainData.labels[shuffle[i]]
+end
+image.display{image=trainData.data[{{1,128}}], nrow=16, zoom=2, legend = 'New Train Data'}
+
+local shuffle = torch.randperm(testData:size())
+oldTestData = testData
+teSize = 4860
+testData = {
+   data = torch.Tensor(teSize, ivch,desImaX,desImaY),
+   labels = torch.Tensor(teSize),
+   size = function() return teSize end
+}
+for i = 1,teSize do
+   testData.data[i] = oldTestData.data[shuffle[i]]
+   testData.labels[i] = oldTestData.labels[shuffle[i]]
+end
+image.display{image=testData.data[{{1,128}}], nrow=16, zoom=2, legend = 'New Test Data'}
 
 -- Displaying the dataset architecture ---------------------------------------
 print('Training Data:')
